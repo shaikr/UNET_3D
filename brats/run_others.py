123fed15fac6f75713f82fc1292177fc3630f3d2
ShaiK <shaikveller@gmail.com>
2019-08-15 11:18:59 +0300

    Overlap factors experiment script

--- brats/run_others.py
+++ brats/run_others.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 #exp_names = os.listdir('/datadrive/configs')
-overlap_factors = [0.1, 0.3, 0.5, 0.7, 0.9]
+overlap_factors = [0.15, 0.35, 0.55, 0.75, 0.95]
 exp_names = ["trainset_experiment_0_val_20_train_0_iter", "trainset_experiment_1_val_20_train_0_iter", "trainset_experiment_2_val_20_train_0_iter"]
 for e in exp_names:
     conf_dir = '../../../../../datadrive/configs/' + e
@@ -13,6 +13,6 @@ for e in exp_names:
     # print(cmd)
     # os.system(cmd)
     for o_f in overlap_factors:
-        cmd = f"python predict.py --split='test' --config='{conf_dir} --overlap_factor={o_f}'"
+        cmd = f"python predict.py --split='test' --config='{conf_dir}' --overlap_factor={o_f}"
         print(cmd)
         os.system(cmd)
