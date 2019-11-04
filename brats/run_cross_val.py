ae61371e22cf1a70a53f71434f8c5b0efeae09b1
ShaiK <shaikveller@gmail.com>
2019-11-04 23:53:28 +0200

    Updated cross-val train script

--- brats/run_cross_val.py
+++ brats/run_cross_val.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import random
 import pickle
+import tables
 import os
 
 def pickle_dump(item, out_file):
@@ -10,50 +11,68 @@ def pickle_dump(item, out_file):
 
 
 def delete_existing_files():
-    os.remove(r"/home/Shai/PycharmProjects/UNET_3D/debug_split/validation_ids.pkl")
-    os.remove(r"/home/Shai/PycharmProjects/UNET_3D/debug_split/training_ids.pkl")
-    os.remove(r"/home/Shai/PycharmProjects/UNET_3D/debug_split/test_ids.pkl")
+    os.remove(r"/data/home/Shai/UNET_3D_NEW/debug_split/validation_ids.pkl")
+    os.remove(r"/data/home/Shai/UNET_3D_NEW/debug_split/training_ids.pkl")
+    os.remove(r"/data/home/Shai/UNET_3D_NEW/debug_split/test_ids.pkl")
 
 
 def create_files(training_list, validation_list, test_list):
-    pickle_dump(training_list, r"/home/Shai/PycharmProjects/UNET_3D/debug_split/training_ids.pkl")
-    pickle_dump(validation_list, r"/home/Shai/PycharmProjects/UNET_3D/debug_split/validation_ids.pkl")
-    pickle_dump(test_list, r"/home/Shai/PycharmProjects/UNET_3D/debug_split/test_ids.pkl")
+    pickle_dump(training_list, r"/data/home/Shai/UNET_3D_NEW/debug_split/training_ids.pkl")
+    pickle_dump(validation_list, r"/data/home/Shai/UNET_3D_NEW/debug_split/validation_ids.pkl")
+    pickle_dump(test_list, r"/data/home/Shai/UNET_3D_NEW/debug_split/test_ids.pkl")
 
 
-all_list = [0, 10, 20, 6, 12, 16, 1, 3, 5, 9, 13, 14, 15, 17, 18, 21, 22, 23, 24, 25, 26, 19, 11, 8, 7, 4, 2]
-random.shuffle(all_list)
-nb_samples = 27
-sample_list = list(range(nb_samples))
-all_list_temp = all_list
+def run_cross_val_training(existing_data_file_path, exp_names_prefix, conf_to_imitate=None):
+    data_file = tables.open_file(existing_data_file_path, "r")
+    all_list = list(range(len(data_file.root.subject_ids)))
+    test_list = [5, 6, 23]  # all_list_temp[:3]
+    print('# of subjects: {}'.format(len(all_list)))
+    random.shuffle(all_list)
+    all_list_temp = all_list
+    all_list_temp.remove(5)
+    all_list_temp.remove(6)
+    all_list_temp.remove(23)
 
-for i in range(9):
-    print(f"In round {i+1} out of 9")
+    all_experiement_names = []
 
-    test_list = all_list_temp[:3]
-    validation_list = all_list_temp[3:6]
-    training_list = all_list_temp[6:]
+    # TODO: currently assumes 27 scans
+    for i in range(9):
+        print(f"In round {i+1} out of 9")
 
-    try:
-        delete_existing_files()
-    except:
-        print('Problem deleting files')
+        validation_list = all_list_temp[:3]
+        training_list = all_list_temp[3:]
 
-    try:
-        create_files(training_list, validation_list, test_list)
-    except:
-        print('Problem deleting files')
+        try:
+            delete_existing_files()
+        except:
+            print('Problem deleting files')
 
-    print('Created files, now training')
-    cmd = f"python train_fetal.py --experiment_name='cross_val_train_{i+1}'"
-    print(cmd)
-    os.system(cmd)
+        try:
+            create_files(training_list, validation_list, test_list)
+        except:
+            print('Problem deleting files')
 
-    print("Finished training, now running on test")
-    conf_dir = '../../../../../datadrive/configs/' + f'cross_val_train_{i+1}'
-    cmd = f"python predict.py --split='test' --config='{conf_dir}'"
-    print(cmd)
-    os.system(cmd)
+        print('Created files, now training')
+        if conf_to_imitate is None:
+            cmd = f"python train_fetal.py --experiment_name='{exp_names_prefix}_cross_val_train_{i+1}'"
+        else:
+            cmd = f"python train_fetal.py --experiment_name='{exp_names_prefix}_cross_val_train_{i+1}'" \
+                  f" --imitate_experiment='{conf_to_imitate}"
+        print(cmd)
+        os.system(cmd)
 
-    print('Finished forward')
-    all_list_temp = all_list_temp[3:] + all_list_temp[:3]
+        print("Finished training, now running on test")
+        conf_dir = '../../../../../datadrive/configs/' + f'{exp_names_prefix}_cross_val_train_{i+1}'
+        cmd = f"python predict.py --split='test' --config='{conf_dir}'"
+        print(cmd)
+        os.system(cmd)
+
+        print('Finished forward')
+        all_list_temp = all_list_temp[3:] + all_list_temp[:3]
+        all_experiement_names = all_experiement_names + [f'{exp_names_prefix}_cross_val_train_{i+1}']
+    return all_experiement_names
+
+
+conf_to_imitate = r"new_arch_exp_64_64_5"
+existing_data_fpath = r"/datadrive/configs/dump_folder/new_arch_exp_64_64_5/fetal_data.h5"
+exp_names_prefix = r"64_64_5_cross_val_train"
