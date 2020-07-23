ba0129b15592fcc4d02f4c2abf41d15c93e13e1d
Shai <shaikveller@gmail.com>
2020-07-23 15:06:12 +0000

    runs other experiment predictions

--- brats/run_others.py
+++ brats/run_others.py
@@ -1,18 +1,22 @@
 import os
 from pathlib import Path
-
+import glob
 
 #exp_names = os.listdir('/datadrive/configs')
 overlap_factors = [0.15, 0.35, 0.55, 0.75, 0.95]
-exp_names = ["trainset_experiment_0_val_20_train_0_iter", "trainset_experiment_1_val_20_train_0_iter", "trainset_experiment_2_val_20_train_0_iter"]
+exp_names = ["20200526_normalization_each_minmax", "20200526_normalization_each_clip_and", "20200526_normalization_each_stretch_and"]
+exp_names = ["20200619_3d_unet_attempts_with_pred_00", "20200621_3d_unet_attempts_with_pred_later_slices_00"]
 for e in exp_names:
     conf_dir = '../../../../../datadrive/configs/' + e
+    #os.rename(os.path.join(conf_dir, 'fetal_data.h5'), os.path.join(conf_dir, 'fetal_data_original.h5'))
+    #os.rename(os.path.join(conf_dir, 'april_data.h5'), os.path.join(conf_dir, 'fetal_data.h5'))
     #conf_dir='../configs/' + e
     #if not Path(os.path.join(conf_dir, 'predictions')).exists():
-    # cmd = f"python predict.py --split='val' --config='{conf_dir}'"
-    # print(cmd)
-    # os.system(cmd)
-    for o_f in overlap_factors:
-        cmd = f"python predict.py --split='test' --config='{conf_dir}' --overlap_factor={o_f}"
-        print(cmd)
-        os.system(cmd)
+    cmd = "python predict.py --split='test' --config='{}'".format(conf_dir)
+    print(cmd)
+    os.system(cmd)
+
+    # for o_f in overlap_factors[:1]:
+    #     cmd = "python predict.py --split='test' --config='{}' --overlap_factor={}".format(conf_dir, o_f)
+    #     print(cmd)
+    #     os.system(cmd)
