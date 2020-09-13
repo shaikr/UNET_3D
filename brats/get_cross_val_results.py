6c5fc8cbc58d92298180ad6100e70447f7fe6e32
ShaiK <shaikveller@gmail.com>
2020-09-14 00:16:46 +0300

    Using np variants of metrics (because types)

--- brats/get_cross_val_results.py
+++ brats/get_cross_val_results.py
@@ -1,6 +1,6 @@
 import nibabel as nib
 import os
-from fetal_net.metrics import dice_coefficient, vod_coefficient, get_surface_distances, \
+from fetal_net.metrics import dice_coefficient_np, vod_coefficient_np, get_surface_distances, \
     false_negative_rate, false_positive_rate
 from fetal_net.postprocess import postprocess_prediction
 import glob
@@ -12,7 +12,7 @@ data_folder = r"/home/Shai/placenta_data/"
 exp_name_prefix = "20200911_single_res_new_data_cross_val_train_"
 output_file_path_volume = os.path.join(configs_folder, exp_name_prefix) + 'volume_results.xlsx'
 
-all_rel_exps = glob.glob(os.path.join(configs_folder, exp_name_prefix, '*'))
+all_rel_exps = glob.glob(os.path.join(configs_folder, exp_name_prefix + '*'))
 subjects_results_dict = {}
 
 
@@ -27,8 +27,8 @@ for exp in all_rel_exps:
         binary_pred = postprocess_prediction(pred)
         truth = nib.load(os.path.join(data_folder, sub_id, 'truth.nii')).get_data()
 
-        cur_dice = dice_coefficient(truth, binary_pred)
-        cur_vo = vod_coefficient(truth, binary_pred)
+        cur_dice = dice_coefficient_np(truth, binary_pred)
+        cur_vo = vod_coefficient_np(truth, binary_pred)
         cur_haus, cur_assd = get_surface_distances(truth, binary_pred)
         fnr = false_negative_rate(truth, binary_pred)
         fpr = false_positive_rate(truth, binary_pred)
