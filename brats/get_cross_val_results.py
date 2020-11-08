70980deb143f734bdc0cf7d88476ed640b67c4d3
ShaiK <shaikveller@gmail.com>
2020-11-08 09:43:07 +0000

    changed metrics to recall and precision + option to save results in different folder

--- brats/get_cross_val_results.py
+++ brats/get_cross_val_results.py
@@ -9,20 +9,20 @@ import pandas as pd
 import argparse
 
 
-def main(configs_folder, data_folder, exp_name_prefix):
-    output_file_path_volume = os.path.join(configs_folder, exp_name_prefix) + 'volume_results.xlsx'
-    output_file_path_area = os.path.join(configs_folder, exp_name_prefix) + 'area_results.xlsx'
+def main(configs_folder, data_folder, results_folder, exp_name_prefix):
+    output_file_path_volume = os.path.join(results_folder, exp_name_prefix) + '_volume_results.xlsx'
+    output_file_path_area = os.path.join(results_folder, exp_name_prefix) + '_area_results.xlsx'
     all_rel_exps = glob.glob(os.path.join(configs_folder, exp_name_prefix + '*'))
     subjects_vol_results_dict = {}
     subjects_area_results_dict = {}
 
     subjects_vol_results = pd.DataFrame(columns=['EXPERIMENT_NAME', 'TEST_ID', 'NSLICES', 'VO_COEFF', 'DICE_COEFF',
-                                                 'HAUSDORFF_DISTANCE', 'ASSD', 'RECALL', 'FPR'])
+                                                 'HAUSDORFF_DISTANCE', 'ASSD', 'RECALL', 'PRECISION'])
 
     subjects_area_results = pd.DataFrame(columns=['EXPERIMENT_NAME', 'TEST_ID', 'ISBLANKS', 'NSLICES',
                                                   'MEAN_VO_COEFF', 'STD_VO_COEFF',
                                                   'MEAN_DICE_COEFF', 'STD_DICE_COEFF',
-                                                  'MEAN_RECALL', 'MEAN_FPR'])
+                                                  'MEAN_RECALL', 'MEAN_PRECISION'])
 
     cur_results_row_vol = 0
     cur_results_row_area = 0
@@ -39,31 +39,27 @@ def main(configs_folder, data_folder, exp_name_prefix):
                 pred_im = nib.load(os.path.join(pred_dir, sub_id, 'prediction.nii.gz'))
                 pred = pred_im.get_data()
                 binary_pred = postprocess_prediction(pred)
-                #save_path = os.path.join(data_folder, sub_id, f'{exp_base}_bin_pred.nii')
-                #os.remove(save_path)
-                #save_path = os.path.join(data_folder, sub_id, f'prediction_2d_depth3_bin_pred.nii')
-                #nib.save(nib.Nifti1Image(binary_pred, pred_im.affine, header=pred_im.header), save_path)
                 truth = nib.load(os.path.join(data_folder, sub_id, 'truth.nii')).get_data()
 
                 ### VOLUME METRICS (not slice-wise) ###
                 cur_dice = dice_coefficient_np(truth, binary_pred)
                 cur_vo = vod_coefficient_np(truth, binary_pred)
                 cur_haus, cur_assd = get_surface_distances(truth.astype('uint8'), binary_pred.astype('uint8'))
-                fnr = recall_rate(truth, binary_pred)
-                fpr = false_positive_rate(truth, binary_pred)
+                rec = recall_rate(truth, binary_pred)
+                prec = 1 - false_positive_rate(truth, binary_pred)
 
                 subjects_vol_results.loc[cur_results_row_vol] = [os.path.basename(exp), sub_id, truth.shape[-1], cur_vo,
-                                                                 cur_dice, cur_haus, cur_assd, fnr, fpr]
+                                                                 cur_dice, cur_haus, cur_assd, rec, prec]
                 cur_results_row_vol += 1
 
                 ### AREA METRICS - all ###
                 dice_coeffs = [dice_coefficient_np(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
                 vods = [vod_coefficient_np(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
-                fnrs = [recall_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
-                fprs = [false_positive_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
+                recs = [recall_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
+                precs = [1 - false_positive_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
                 subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, -1, truth.shape[-1],
                                                                    np.mean(vods), np.std(vods), np.mean(dice_coeffs),
-                                                                   np.std(dice_coeffs), np.mean(fnrs), np.mean(fprs)]
+                                                                   np.std(dice_coeffs), np.mean(recs), np.mean(precs)]
                 cur_results_row_area += 1
 
                 truth_sums = np.sum(np.sum(truth, axis=0), axis=0)
@@ -76,8 +72,8 @@ def main(configs_folder, data_folder, exp_name_prefix):
                                                                    np.std([vods[i] for i in non_blank_inds]),
                                                                    np.mean([dice_coeffs[i] for i in non_blank_inds]),
                                                                    np.std([dice_coeffs[i] for i in non_blank_inds]),
-                                                                   np.mean([fnrs[i] for i in non_blank_inds]),
-                                                                   np.mean([fprs[i] for i in non_blank_inds])]
+                                                                   np.mean([recs[i] for i in non_blank_inds]),
+                                                                   np.mean([precs[i] for i in non_blank_inds])]
                 cur_results_row_area += 1
 
                 ### AREA METRICS - blanks ###
@@ -86,8 +82,8 @@ def main(configs_folder, data_folder, exp_name_prefix):
                                                                    np.std([vods[i] for i in blank_inds]),
                                                                    np.mean([dice_coeffs[i] for i in blank_inds]),
                                                                    np.std([dice_coeffs[i] for i in blank_inds]),
-                                                                   np.mean([fnrs[i] for i in blank_inds]),
-                                                                   np.mean([fprs[i] for i in blank_inds])]
+                                                                   np.mean([recs[i] for i in blank_inds]),
+                                                                   np.mean([precs[i] for i in blank_inds])]
                 cur_results_row_area += 1
             except Exception as e:
                 print(e)
@@ -105,10 +101,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--configs_folder", help="specifies config folder path",
                         type=str, required=False, default=r"/datadrive/configs")
+    parser.add_argument("--results_folder", help="specifies config folder path",
+                        type=str, required=False, default=r"/datadrive/results")
     parser.add_argument("--data_folder", help="where is the relevant data",
                         type=str, required=False, default=r"/home/Shai/placenta_data/")
     parser.add_argument("--prefix", help="specifies what experiments to go over by their prefix",
                         type=str, required=True)
     opts = parser.parse_args()
 
-    main(opts.configs_folder, opts.data_folder, opts.prefix)
+    main(opts.configs_folder, opts.data_folder, opts.results_folder, opts.prefix)
