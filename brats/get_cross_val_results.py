37d50f582533733bb988bd56e0e461a02e92b8b6
ShaiK <shaikveller@gmail.com>
2020-11-10 16:37:36 +0000

    added registration of multiple predictions per subject id

--- brats/get_cross_val_results.py
+++ brats/get_cross_val_results.py
@@ -34,57 +34,66 @@ def main(configs_folder, data_folder, results_folder, exp_name_prefix):
             print(f"Skipping: {exp}")
             continue
         sub_ids = os.listdir(pred_dir)
-        for sub_id in sub_ids:
+        for s_id in sub_ids:
             try:
-                pred_im = nib.load(os.path.join(pred_dir, sub_id, 'prediction.nii.gz'))
-                pred = pred_im.get_data()
-                binary_pred = postprocess_prediction(pred)
-                truth = nib.load(os.path.join(data_folder, sub_id, 'truth.nii')).get_data()
+                truth = nib.load(os.path.join(data_folder, s_id, 'truth.nii')).get_data()
+                print(truth.shape)
+                preds = glob.glob(os.path.join(pred_dir, s_id, 'prediction*'))
+                for i, pred_path in enumerate(preds):
+                    if os.path.basename(pred_path)[-8] == 'n':
+                        sub_id = s_id + '_0'
+                    else:
+                        sub_id = s_id + f'_{os.path.basename(pred_path)[-8]}'
+                    pred_im = nib.load(pred_path)
+                    pred = pred_im.get_data()
+                    if pred.ndim > truth.ndim:
+                        pred = np.mean(pred, axis=0)
+                    binary_pred = postprocess_prediction(pred)
 
-                ### VOLUME METRICS (not slice-wise) ###
-                cur_dice = dice_coefficient_np(truth, binary_pred)
-                cur_vo = vod_coefficient_np(truth, binary_pred)
-                cur_haus, cur_assd = get_surface_distances(truth.astype('uint8'), binary_pred.astype('uint8'))
-                rec = recall_rate(truth, binary_pred)
-                prec = 1 - false_positive_rate(truth, binary_pred)
+                    ### VOLUME METRICS (not slice-wise) ###
+                    cur_dice = dice_coefficient_np(truth, binary_pred)
+                    cur_vo = vod_coefficient_np(truth, binary_pred)
+                    cur_haus, cur_assd = get_surface_distances(truth.astype('uint8'), binary_pred.astype('uint8'))
+                    rec = recall_rate(truth, binary_pred)
+                    prec = 1 - false_positive_rate(truth, binary_pred)
 
-                subjects_vol_results.loc[cur_results_row_vol] = [os.path.basename(exp), sub_id, truth.shape[-1], cur_vo,
-                                                                 cur_dice, cur_haus, cur_assd, rec, prec]
-                cur_results_row_vol += 1
+                    subjects_vol_results.loc[cur_results_row_vol] = [os.path.basename(exp), sub_id, truth.shape[-1], cur_vo,
+                                                                     cur_dice, cur_haus, cur_assd, rec, prec]
+                    cur_results_row_vol += 1
+                    
+                    ### AREA METRICS - all ###
+                    dice_coeffs = [dice_coefficient_np(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
+                    vods = [vod_coefficient_np(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
+                    recs = [recall_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
+                    precs = [1 - false_positive_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
+                    subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, -1, truth.shape[-1],
+                                                                       np.mean(vods), np.std(vods), np.mean(dice_coeffs),
+                                                                       np.std(dice_coeffs), np.mean(recs), np.mean(precs)]
+                    cur_results_row_area += 1
 
-                ### AREA METRICS - all ###
-                dice_coeffs = [dice_coefficient_np(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
-                vods = [vod_coefficient_np(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
-                recs = [recall_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
-                precs = [1 - false_positive_rate(truth[:, :, i], binary_pred[:, :, i]) for i in range(truth.shape[-1])]
-                subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, -1, truth.shape[-1],
-                                                                   np.mean(vods), np.std(vods), np.mean(dice_coeffs),
-                                                                   np.std(dice_coeffs), np.mean(recs), np.mean(precs)]
-                cur_results_row_area += 1
+                    truth_sums = np.sum(np.sum(truth, axis=0), axis=0)
+                    non_blank_inds = [i for i in range(truth.shape[-1]) if truth_sums[i] > 0]
+                    blank_inds = [i for i in range(truth.shape[-1]) if truth_sums[i] == 0]
+                    
+                    ### AREA METRICS - non-blanks ###
+                    subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, 0, len(non_blank_inds),
+                                                                       np.mean([vods[i] for i in non_blank_inds]),
+                                                                       np.std([vods[i] for i in non_blank_inds]),
+                                                                       np.mean([dice_coeffs[i] for i in non_blank_inds]),
+                                                                       np.std([dice_coeffs[i] for i in non_blank_inds]),
+                                                                       np.mean([recs[i] for i in non_blank_inds]),
+                                                                       np.mean([precs[i] for i in non_blank_inds])]
+                    cur_results_row_area += 1
 
-                truth_sums = np.sum(np.sum(truth, axis=0), axis=0)
-                non_blank_inds = [i for i in range(truth.shape[-1]) if truth_sums[i] > 0]
-                blank_inds = [i for i in range(truth.shape[-1]) if truth_sums[i] == 0]
-
-                ### AREA METRICS - non-blanks ###
-                subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, 0, len(non_blank_inds),
-                                                                   np.mean([vods[i] for i in non_blank_inds]),
-                                                                   np.std([vods[i] for i in non_blank_inds]),
-                                                                   np.mean([dice_coeffs[i] for i in non_blank_inds]),
-                                                                   np.std([dice_coeffs[i] for i in non_blank_inds]),
-                                                                   np.mean([recs[i] for i in non_blank_inds]),
-                                                                   np.mean([precs[i] for i in non_blank_inds])]
-                cur_results_row_area += 1
-
-                ### AREA METRICS - blanks ###
-                subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, 1, len(blank_inds),
-                                                                   np.mean([vods[i] for i in blank_inds]),
-                                                                   np.std([vods[i] for i in blank_inds]),
-                                                                   np.mean([dice_coeffs[i] for i in blank_inds]),
-                                                                   np.std([dice_coeffs[i] for i in blank_inds]),
-                                                                   np.mean([recs[i] for i in blank_inds]),
-                                                                   np.mean([precs[i] for i in blank_inds])]
-                cur_results_row_area += 1
+                    ### AREA METRICS - blanks ###
+                    subjects_area_results.loc[cur_results_row_area] = [os.path.basename(exp), sub_id, 1, len(blank_inds),
+                                                                       np.mean([vods[i] for i in blank_inds]),
+                                                                       np.std([vods[i] for i in blank_inds]),
+                                                                       np.mean([dice_coeffs[i] for i in blank_inds]),
+                                                                       np.std([dice_coeffs[i] for i in blank_inds]),
+                                                                       np.mean([recs[i] for i in blank_inds]),
+                                                                       np.mean([precs[i] for i in blank_inds])]
+                    cur_results_row_area += 1
             except Exception as e:
                 print(e)
                 continue
